8bef7f0688a6eb5e548f0c2b3f85f2f5bf804df3
shadiaali <shadiamalfoy@gmail.com>
2019-03-20 05:32:43 -0400

    icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,14 @@
   <v-btn class="pink white--text">click me</v-btn>
   <v-btn depressed color="blue" class="white--text">click me again</v-btn>
   <v-btn flat color="pink">another button</v-btn>
-  
+  <!--you can use icon keywords-->
+<v-btn depressed small class="pink white--text" ><v-icon left>email</v-icon  ><span>email me</span></v-btn>
+
+<v-btn depressed large class="pink white--text" ><v-icon left>email</v-icon  ><span>email me</span></v-btn>
+
+
+<v-btn fab flat small dark color="purple white--text" ><v-icon left>favorite</v-icon  ><span>email me</span></v-btn>
+
   </div>
 </template>
 
