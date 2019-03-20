a6246d9e00643a1db241db350f11f84363fcb51c
shadiaali <shadiamalfoy@gmail.com>
2019-03-20 05:41:49 -0400

    breakpoints and visibility classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
     <h1>home page</h1>
-    
-  <v-btn class="pink white--text">click me</v-btn>
-  <v-btn depressed color="blue" class="white--text">click me again</v-btn>
-  <v-btn flat color="pink">another button</v-btn>
-  <!--you can use icon keywords-->
-<v-btn depressed small class="pink white--text" ><v-icon left>email</v-icon  ><span>email me</span></v-btn>
+    <!--hide on medium and down screens-->
+   <v-btn class="hidden-md-and-down">click me</v-btn> 
 
-<v-btn depressed large class="pink white--text" ><v-icon left>email</v-icon  ><span>email me</span></v-btn>
-
-
-<v-btn fab flat small dark color="purple white--text" ><v-icon left>favorite</v-icon  ><span>email me</span></v-btn>
-
+<v-btn class="pink hidden-md-and-up">click me</v-btn> 
   </div>
 </template>
 
