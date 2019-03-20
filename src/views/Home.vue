8f75ee8a9b9300cf4a41d29aa984193d382157bc
shadiaali <shadiamalfoy@gmail.com>
2019-03-20 05:26:21 -0400

    buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="home">
     <h1>home page</h1>
-    <!--utility classes can be added to tags, along with specifications (white--text)-->
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea soluta, quidem porro rem sit, cum similique cupiditate esse saepe impedit magnam libero. Quas distinctio non minus tenetur possimus? Nisi, tenetur!</p>
-    <!--you can also use lighten/darken additions, 1-4-->
-    <p class="pink lighten-3 black--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, autem at voluptates soluta voluptatum placeat inventore nihil cupiditate, doloribus exercitationem facilis libero eius fuga ad est rerum odio sapiente commodi.</p>
-    <!--display [fonts] class ranges 1-4-->
-    <h1 class="display-4">Massive Display</h1>
-    <p class="headline">this is a headline</p>
-    <p class="subheading">subheading</p>
-    <p class="caption">this is a caption</p>
+    
+  <v-btn class="pink white--text">click me</v-btn>
+  <v-btn depressed color="blue" class="white--text">click me again</v-btn>
+  <v-btn flat color="pink">another button</v-btn>
+  
   </div>
 </template>
 
