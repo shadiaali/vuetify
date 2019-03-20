648792e131b336be0863c1eeda37975955d2836c
shadiaali <shadiamalfoy@gmail.com>
2019-03-20 05:21:11 -0400

    fonts and colours

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
   <!--root vuetify component. all other components should be nested inside-->
   <v-app>
-    <!--a vuetify component. When loaded in the template, it renders some html. Can have props (like classes) added for functionality: eg, light/dark. Different props which have values can also be added. For instance, height, There is also a load of classes that can be added right from material design out of the box, such as colours e.g, indigo-->
-    <v-toolbar app dark height="50" class="red">
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   data () {
     return {
       //
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>home page</h1>
+    <!--utility classes can be added to tags, along with specifications (white--text)-->
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea soluta, quidem porro rem sit, cum similique cupiditate esse saepe impedit magnam libero. Quas distinctio non minus tenetur possimus? Nisi, tenetur!</p>
+    <!--you can also use lighten/darken additions, 1-4-->
+    <p class="pink lighten-3 black--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, autem at voluptates soluta voluptatum placeat inventore nihil cupiditate, doloribus exercitationem facilis libero eius fuga ad est rerum odio sapiente commodi.</p>
+    <!--display [fonts] class ranges 1-4-->
+    <h1 class="display-4">Massive Display</h1>
+    <p class="headline">this is a headline</p>
+    <p class="subheading">subheading</p>
+    <p class="caption">this is a caption</p>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  //import HelloWorld from '../components/HelloWorld'
 
   export default {
-    components: {
-      HelloWorld
-    }
+    
   }
 </script>
